f94a2e6aad18c1216838563657f645a112de1846
aflaogonzalez <a.flaogonzalez@uandresbello.edu>
2025-11-01 16:24:09 -0300

    Se crea producto.KT para comenzar a trabajar los productos

---
+++ app/src/main/java/cl/lte/ae4_abpro/Product.kt
@@ -0,0 +1,12 @@
+package cl.lte.ae4_abpro
+
+/**
+ * Representa un producto en el supermercado.
+ *
+ * @property name El nombre del producto.
+ * @property price El precio del producto en formato de texto.
+ */
+data class Product(
+    val name: String,
+    val price: String
+)
