34abed588836ba87ffe2610e8276fef39bc35e0e
aflaogonzalez <a.flaogonzalez@uandresbello.edu>
2025-11-01 16:17:58 -0300

    Primeros pasos del trabajo con recicleView

--- app/src/main/java/cl/lte/ae4_abpro/MainActivity.kt
+++ app/src/main/java/cl/lte/ae4_abpro/MainActivity.kt
@@ -1,20 +1,19 @@
 package cl.lte.ae4_abpro
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // 1. Encontrar la Toolbar que agregamos en el layout activity_main.xml
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+
+        // 2. Establecer la Toolbar como la ActionBar para esta Activity.
+        //    Esto nos permite personalizarla y responder a sus eventos.
+        setSupportActionBar(toolbar)
     }
 }
